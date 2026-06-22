8775eb142578f772f331951a0d2853525c72ea7f
apervashov <videoman496@gmail.com>
2026-06-22 15:34:26 +0200

    feat: add identifier validation for OTP request

--- src/modules/auth/dto/requests/send-otp.request.ts
+++ src/modules/auth/dto/requests/send-otp.request.ts
@@ -1,4 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger"
+import { IsEnum, IsString, Validate } from "class-validator"
+import { IdentifierValidator } from "src/shared/validators/identifier.validator"
+
 export class SendOtpRequest {
+    @ApiProperty({
+        description: 'The identifier of the user',
+        examples: ['+5511999999', 'test@example.com'],
+    })
+    @IsString()
+    @Validate(IdentifierValidator)
     public identifier: string
+    @ApiProperty({
+        description: 'The type of the identifier',
+        enum: ['phone', 'email'],
+        examples: ['phone', 'email'],
+    })
+    @IsEnum(['phone', 'email'])
     public type: 'phone' | 'email'
 }
---
+++ src/shared/validators/identifier.validator.ts
@@ -0,0 +1,33 @@
+import { ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface } from "class-validator"
+import { SendOtpRequest } from "src/modules/auth/dto/requests/send-otp.request"
+
+@ValidatorConstraint({ name: 'identifierValidator', async: false })
+export class IdentifierValidator implements ValidatorConstraintInterface{
+    public validate(value: string, args?: ValidationArguments): boolean {
+       const object = args?.object as SendOtpRequest
+
+       if(object.type === 'email'){
+        return (
+            typeof value === 'string' && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)
+        )
+       }
+       else if(object.type === 'phone'){
+        return (
+            typeof value === 'string' && /^[0-9]{10}$/.test(value)
+        )
+       }
+       return false
+    }
+
+    public defaultMessage(args?: ValidationArguments): string {
+        const object = args?.object as SendOtpRequest
+
+        if(object.type === 'email'){
+            return 'Email is not valid'
+        }
+        else if(object.type === 'phone'){
+            return 'Phone is not valid'
+        }
+        return 'Identifier is not valid'
+    }
+}
